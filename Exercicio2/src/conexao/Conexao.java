9150092f853179850488259fad84ededda33d987
Daniella Silva <164039086+emilydaniella@users.noreply.github.com>
2025-02-21 09:02:17 -0300

    Update Conexao.java

--- Exercicio2/src/conexao/Conexao.java
+++ Exercicio2/src/conexao/Conexao.java
@@ -4,12 +4,12 @@ import java.sql.SQLException;
 
 public class Conexao {
     private static final String URL = "jdbc:postgresql://localhost:5432/teste";
-    private static final String USUARIO = "postgres"; // Substitua pelo seu usuário do PostgreSQL
-    private static final String SENHA = "daani840"; // Substitua pela senha do PostgreSQL
+    private static final String USUARIO = "postgres"; 
+    private static final String SENHA = "dani123"; 
 
     public static Connection conectar() {
         try {
-            Class.forName("org.postgresql.Driver"); // Garante que o driver seja carregado
+            Class.forName("org.postgresql.Driver"); 
             return DriverManager.getConnection(URL, USUARIO, SENHA);
         } catch (ClassNotFoundException e) {
             System.out.println(" Driver JDBC não encontrado: " + e.getMessage());
